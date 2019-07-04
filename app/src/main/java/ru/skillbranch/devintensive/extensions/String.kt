d0a03bbd8e3fe42163d0a11661837ca4c1e7ed20
Gaipov Murad <m.gaipov@mail.ru>
2019-07-05 01:44:18 +0300

    added truncate and stripHtml

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -0,0 +1,10 @@
+package ru.skillbranch.devintensive.extensions
+
+
+
+fun String.truncate(trunk:Int = 16):String{
+    val str = this.dropLast(this.length - trunk).trimEnd()
+    return str.padEnd(str.length+3,'.')
+}
+
+fun String.stripHtml() = this.replace(Regex("<[^<]*?>|&\\d+;"), "").replace(Regex("\\s+"), " ")
