506a4a08f02c036304f6fb547d4216283fdb6d29
Gaipov Murad <m.gaipov@mail.ru>
2019-07-14 16:23:59 +0300

    comleted all tasks

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -60,7 +60,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener,TextView.OnEditor
 
     override fun onClick(v: View?) {
         if(v?.id == R.id.iv_send){
-            val (phrase,color) = benderObj.listenAnswer(messageEt.text.toString().toLowerCase())
+            val (phrase,color) = benderObj.listenAnswer(messageEt.text.toString())
             messageEt.setText("")
             val(r,g,b) = color
             benderImage.setColorFilter(Color.rgb(r,g,b),PorterDuff.Mode.MULTIPLY)
--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -1,6 +1,7 @@
 package ru.skillbranch.devintensive.models
 
 import android.util.Log
+import android.view.View.Z
 
 class Bender(var status: Status = Status.NORMAL, var question: Question = Question.NAME) {
 
@@ -15,18 +16,23 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
     }
 
     fun listenAnswer(answer: String):Pair<String, Triple<Int,Int,Int>>{
-        return if (question.answers.contains(answer)) {
-            question = question.nextQuestion()
-            "Отлично - ты справился\n${question.question}" to status.color
-        } else {
-            if (status == Status.CRITICAL) {
-                status = Status.NORMAL
-                question = Question.NAME
-                "Это неправильный ответ. Давай все по новой\n${question.question}" to status.color
+        return if(question.validation(answer)){
+             answer.toLowerCase()
+            if (question.answers.contains(answer)) {
+                question = question.nextQuestion()
+                "Отлично - ты справился\n${question.question}" to status.color
             } else {
-                status = status.nextStatus()
-                "Это неправильный ответ\n${question.question}" to status.color
+                if (status == Status.CRITICAL) {
+                    status = Status.NORMAL
+                    question = Question.NAME
+                    "Это неправильный ответ. Давай все по новой\n${question.question}" to status.color
+                } else {
+                    status = status.nextStatus()
+                    "Это неправильный ответ\n${question.question}" to status.color
+                }
             }
+        }else{
+           return "${question.mistake}\n${question.question}" to status.color
         }
 
     }
@@ -48,28 +54,53 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
 
 
 
-    enum class Question(val question: String, val answers: List<String>) {
-        NAME("Как меня зовут?", listOf("Бендер", "bender")) {
+    enum class Question(val question: String, val answers: List<String>,val mistake:String?) {
+        NAME("Как меня зовут?", listOf("Бендер", "Bender"),mistake = "Имя должно начинаться с заглавной буквы") {
+            override fun validation(answer:String):Boolean {
+               return answer[0].isUpperCase()
+            }
+
             override fun nextQuestion(): Question =PROFESSION
         },
-        PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")) {
+        PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender"),mistake = "Профессия должна начинаться со строчной буквы") {
+            override fun validation(answer: String):Boolean {
+                return answer[0].isLowerCase()
+            }
+
             override fun nextQuestion(): Question = MATERIAL
 
 
         },
-        MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")) {
+        MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood"),mistake = "Материал не должен содержать цифр") {
+            override fun validation(answer: String):Boolean {
+              return answer.all{it.isLetter()}
+            }
+
             override fun nextQuestion(): Question = BDAY
         },
-        BDAY("Когда меня создали?", listOf("2993")) {
+        BDAY("Когда меня создали?", listOf("2993"),"Год моего рождения должен содержать только цифры") {
+            override fun validation(answer: String):Boolean {
+                return answer.all { it.isDigit() }
+            }
+
             override fun nextQuestion(): Question = SERIAL
         },
-        SERIAL("Мой серийный номер?", listOf("2716057")) {
+        SERIAL("Мой серийный номер?", listOf("2716057"),"Серийный номер содержит только цифры, и их 7") {
+            override fun validation(answer: String):Boolean {
+                return answer.all { it.isDigit() } && answer.length == 7
+            }
+
             override fun nextQuestion(): Question = IDLE
         },
-        IDLE("На этом все, вопросов больше нет", listOf()) {
+        IDLE("На этом все, вопросов больше нет", listOf(),mistake = null) {
+            override fun validation(answer: String):Boolean {
+                return true
+            }
+
             override fun nextQuestion(): Question = IDLE
         };
 
         abstract fun nextQuestion():Question
+        abstract fun validation(answer:String):Boolean
     }
 }
